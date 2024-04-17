28533b439402ecd320a67ed1367ca3cb2a7a9120
st0q <tssk713@gmail.com>
2024-04-17 23:45:34 +0900

    add visualizer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        plugins: [
+          mode === 'analyze' &&
+            visualizer({
+              open: true,
+              filename: 'dist/stats.html',
+              gzipSize: true,
+              // Brotli
+              //   https://dev.classmethod.jp/articles/update-cloudfront-support-brotli/
+              brotliSize: true,
+            }),
+        ],
+      },
+    },
+  };
 });
