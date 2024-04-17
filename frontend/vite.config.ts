56aaa6dfb2b528d6bd582bebd93608cf1a4ed6e1
st0q <tssk713@gmail.com>
2024-04-17 23:46:00 +0900

    add splitVendorChunkPlugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     build: {
       rollupOptions: {
         plugins: [
@@ -19,6 +19,16 @@ export default defineConfig(({ mode }) => {
               brotliSize: true,
             }),
         ],
+        // output: {
+        //   // entry chunk assets それぞれの書き出し名の指定
+        //   entryFileNames: `assets/[name]-entryFileNames.js`,
+        //   chunkFileNames: `assets/[name]-chunkFileNames.js`,
+        //   assetFileNames: `assets/[name]-assetFileNames.[ext]`,
+        //   manualChunks: {
+        //     vendor: ['react'],
+        //     // dateFormatter: ['date-fns'],
+        //   },
+        // },
       },
     },
   };
